22541e978d7a0d8021e8f8ccf85a464cc5909612
Charlie Jackson <charlieojackson@gmail.com>
2020-09-29 14:24:55 +0100

    fixing state on game

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,21 +2,19 @@ import React, {useState} from 'react';
 
 const boxes      = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
 const winOptions = ['brownie', 'garlic flat bread', 'pizza'];
-let winningBox   = false;
-let winningItem  = false;
 
 const Game = (props) => {
     const [gameOpacity, setGameOpacity] = useState('gameBoxHidden gameBox');
     const [competition, setcompetition] = useState(false);
+    const [winningBox, setwinningBox]   = useState(false)
+    const [winningItem, setwinningItem] = useState(false)
 
     const boxClicked = () => {
-
-        winningBox  = boxes[Math.floor(Math.random() * boxes.length)];
-        winningItem = winOptions[Math.floor(Math.random() * winOptions.length)];
-        
+        setwinningBox(boxes[Math.floor(Math.random() * boxes.length)]);
+        setwinningItem(winOptions[Math.floor(Math.random() * winOptions.length)]);
         setcompetition(true);
-        console.log(' winning item is: ', winningItem, 'and it was under box ', winningBox);
         setGameOpacity('gameBoxClear gameBox');
+
     }
 
     return (
@@ -25,8 +23,8 @@ const Game = (props) => {
             <p>Underneath one of these 10 boxes is a free gift of either <span className='winOption'>a brownie</span>, <span className='winOption'>garlic flat bread</span>, or <span className='winOption'>a pizza of your chosing</span>. Choose your box to see if you a winner!</p>
             <div className='divFlex'>
                 {boxes.map((box, index) => {
-                    if (competition & winningBox==index +1){
-                        return <div className={gameOpacity} key={box} onClick={boxClicked}>{winningItem}</div>
+                    if (competition & parseInt(winningBox) === index +1){
+                        return <div className={gameOpacity} key={box} onClick={boxClicked}><span id='winner'>{winningItem}</span></div>
                     }
                     return <div className={gameOpacity} key={box} onClick={boxClicked}>{box}</div>
                 }
